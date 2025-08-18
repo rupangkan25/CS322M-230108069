08f681a7c9945effbf7f54d273dec1556494b64f
Rupangkan Mazumdar <rupangkan25mazumdar@gmail.com>
2025-08-18 22:45:47 +0530

    Problem-4

---
+++ Assignment-2-P4/link_top.v
@@ -0,0 +1,15 @@
+// link_top.v
+module link_top(
+    input wire clk,
+    input wire rst,
+    output wire done
+);
+    wire req, ack;
+    wire [7:0] data;
+
+    master_fsm u_master(.clk(clk), .rst(rst), .ack(ack),
+                        .req(req), .data(data), .done(done));
+
+    slave_fsm u_slave(.clk(clk), .rst(rst), .req(req), .data_in(data),
+                      .ack(ack), .last_byte());
+endmodule
